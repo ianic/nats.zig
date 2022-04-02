f49cd097aad6403222b05e069bc984c164cbc44f
Igor Anic <ianic@inet.hr>
2022-04-02 17:59:39 +0200

    parse server info message body

--- conn.zig
+++ conn.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
 const net = std.net;
+const Allocator = std.mem.Allocator;
 
 pub fn main() !void {
     const addr = try net.Address.parseIp4("127.0.0.1", 4222);
@@ -100,8 +101,61 @@ const Tokens = enum(u8) {
     _,
 };
 
+
+// TODO check this empty string idea, trying to avoid undefined
+const empty_str = ([_]u8{})[0..];
+
+const ServerInfo = struct {
+    // ref: https://docs.nats.io/reference/reference-protocols/nats-protocol#info
+    // https://github.com/nats-io/nats.go/blob/e076b0dcab3193b8d7cf41c1b747355ad1302170/nats.go#L687
+    server_id: []u8 = empty_str,
+    server_name: []u8 = empty_str,
+    proto: u32 = 1,
+    version: []u8 = empty_str,
+    host: []u8 = empty_str,
+    port: u32 = 4222,
+
+    headers: bool = false,
+    auth_required: bool = false,
+    tls_required: bool = false,
+    tls_available: bool = false,
+
+    max_payload: u64 = 1048576,
+    client_id: u64 = 0,
+    client_ip: []u8 = empty_str,
+
+    nonce: []u8 = ([_]u8{})[0..],
+    cluster: []u8 = empty_str,
+    connect_urls: [][]u8 = ([_][]u8{})[0..],
+
+    ldm: bool = false,
+    jetstream: bool = false,
+
+    pub fn decode(alloc: Allocator, buf: []const u8) std.json.ParseError(ServerInfo)!ServerInfo {
+        // fixing error: evaluation exceeded 1000 backwards branches
+        // ref: https://github.com/ziglang/zig/issues/9728
+        @setEvalBranchQuota(1024 * 8);
+        var stream = std.json.TokenStream.init(buf);
+        return try std.json.parse(ServerInfo, &stream, .{
+            .allocator = alloc,
+            .ignore_unknown_fields = true,
+        });
+    }
+
+    pub fn deinit(self: ServerInfo, alloc: Allocator) void {
+        std.json.parseFree(ServerInfo, self, .{
+            .allocator = alloc,
+        });
+    }
+};
+
+const testing = std.testing;
 const expect = std.testing.expect;
-test "Parser info message" {
+const expectEqual = std.testing.expectEqual;
+const mem = std.mem;
+
+
+test "parser info message" {
     const buf =
         \\INFO {"server_id":"id","server_name":"name","version":"2.8.0","proto":1,"go":"go1.18","host":"0.0.0.0","port":4222,"headers":true,"max_payload":1048576,"jetstream":true,"client_id":53,"client_ip":"127.0.0.1"}
     ;
@@ -163,4 +217,26 @@ test "Parser info message" {
             ParserError.UnexpectedToken => {},
         }
     }
+}
+
+test "decode server info message body" {
+    const buf =
+        \\{"server_id":"id","server_name":"name","version":"2.8.0","proto":1,"go":"go1.18","host":"0.0.0.0","port":4222,"headers":true,"max_payload":123456,"jetstream":true,"client_id":53,"client_ip":"127.0.0.1"}
+    ;
+
+    var alloc = std.testing.allocator;
+    var im = try ServerInfo.decode(alloc, buf);
+    defer im.deinit(alloc);
+
+    try expect(mem.eql(u8, "id", im.server_id));
+    try expect(mem.eql(u8, "name", im.server_name));
+    try expect(mem.eql(u8, "2.8.0", im.version));
+    try expect(mem.eql(u8, "127.0.0.1", im.client_ip));
+    try expect(mem.eql(u8, empty_str, im.nonce));
+    try expectEqual(im.port, 4222);
+    try expectEqual(im.proto, 1);
+    try expectEqual(im.max_payload, 123456);
+    try expectEqual(im.client_id, 53);
+    try expect(im.headers);
+    try expect(im.jetstream);
 }
