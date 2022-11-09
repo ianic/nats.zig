c1d1c3bf158c2b9226013e2d5197398fa64e2d9f
Igor Anić <igor.anic@gmail.com>
2022-11-09 15:40:21 +0100

    add publisher to the ngs example
    It now subscribes publish few messages and show received messages.

--- examples/ngs.zig
+++ examples/ngs.zig
@@ -1,62 +1,57 @@
 const std = @import("std");
 const nats = @import("nats-sync");
 
+pub const scope_levels = [_]std.log.ScopeLevel{
+    .{ .scope = .nats, .level = .info },
+};
+
+// Example usage:
+// ngs my_creds_file.creds my_subject
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    const argv = std.os.argv;
+    if (argv.len < 3) {
+        std.debug.print("USAGE: {s} [creds_file_path] [subject]\n", .{argv[0]});
+        return;
+    }
+    var creds_file_path = std.mem.span(argv[1]);
+    const subject = std.mem.span(argv[2]);
+
     var conn = try nats.connect(allocator, .{
         .host = "connect.ngs.global",
-        .creds_file_path = "/Users/ianic/mantil-io/infrastructure/secrets/logs-listener.creds",
+        .creds_file_path = creds_file_path,
     });
     defer conn.close();
 
-    var sid = try conn.subscribe("_INBOX.foo");
-    std.log.debug("subscribe sid: {d}", .{sid});
-    try conn.unsubscribeBySid(sid);
-    sid = try conn.subscribe("_INBOX.foo");
-    std.log.debug("subscribe sid: {d}", .{sid});
+    _ = try conn.subscribe(subject);
 
+    // start publisher
+    const no_msgs: u8 = 10;
+    var trd = try std.Thread.spawn(.{}, publish, .{ &conn, subject, no_msgs });
+
+    // subscriber
+    var msgs: u8 = 0;
     while (true) {
         if (try conn.read()) |msg| {
-            std.log.debug("got msg {d}", .{msg.payload.?.len});
-        } else {
-            std.log.debug("nothing received", .{});
+            std.log.debug("got msg {d}", .{msg.payload.?[0]});
+            msgs += 1;
+            if (msgs == no_msgs) {
+                break;
+            }
         }
     }
+    trd.join();
 }
 
-// what log level from libs are allowed
-const libsLogLevel = std.log.Level.debug;
-
-pub fn log(
-    comptime level: std.log.Level,
-    comptime scope: @TypeOf(.EnumLiteral),
-    comptime format: []const u8,
-    args: anytype,
-) void {
-    // Ignore all non-error logging from sources other than .default
-    const scope_prefix = switch (scope) {
-        .default => "",
-        else => if (@enumToInt(level) <= @enumToInt(libsLogLevel))
-            "(" ++ @tagName(scope) ++ ") "
-        else
-            return,
-    };
-
-    const prefix = comptime switch (level) {
-        .debug => "[DBG] ",
-        .info => "[INF] ",
-        .warn => "[WRN] ",
-        .err => "[ERR] ",
-    } ++ scope_prefix;
-
-    // Print the message to stderr, silently ignoring any errors
-    std.debug.getStderrMutex().lock();
-    defer std.debug.getStderrMutex().unlock();
-    const stderr = std.io.getStdErr().writer();
-
-    stderr.print("{d} ", .{std.time.milliTimestamp()}) catch return;
-    stderr.print(prefix ++ format ++ "\n", args) catch return;
+fn publish(conn: *nats.Conn, subject: []const u8, no_msgs: u8) void {
+    var i: u8 = 0;
+    while (i < no_msgs) : (i += 1) {
+        conn.publish(subject, &[_]u8{i}) catch {
+            unreachable;
+        };
+    }
 }
